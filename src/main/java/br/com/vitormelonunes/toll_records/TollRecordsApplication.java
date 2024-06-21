9c213211a42f302c9cdb8404cc779df33b1a194d
Vitor de Melo Nunes <vitor.nunes@gruposantander.com>
2024-06-21 18:00:13 -0300

    Initial commit

---
+++ src/main/java/br/com/vitormelonunes/toll_records/TollRecordsApplication.java
@@ -0,0 +1,13 @@
+package br.com.vitormelonunes.toll_records;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TollRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TollRecordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vitormelonunes/toll_records/TollRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vitormelonunes.toll_records;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TollRecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
