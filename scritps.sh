454f5fdd7a7478453d404b5cfd298dcaa1a69c16
Vitor de Melo Nunes <vitor.nunes@gruposantander.com>
2024-06-23 14:26:50 -0300

    feat: Publishing Toll Records Created

--- scritps.sh
+++ scritps.sh
@@ -9,5 +9,11 @@ docker exec -it localstack aws --endpoint-url=http://localhost:4566 sqs create-q
 # Subscriber SQS in SNS
 docker exec -it localstack aws --endpoint-url=http://localhost:4566 sns subscribe --topic-arn arn:aws:sns:sa-east-1:000000000000:toll-records-created --protocol sqs --notification-endpoint arn:aws:sqs:sa-east-1:000000000000:toll-records-payment-pending-queue --region sa-east-1
 
+# Create SQS Topic
+docker exec -it localstack aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name toll-records-created-notifications-pending-queue --region sa-east-1
+
+# Subscriber SQS in SNS
+docker exec -it localstack aws --endpoint-url=http://localhost:4566 sns subscribe --topic-arn arn:aws:sns:sa-east-1:000000000000:toll-records-created --protocol sqs --notification-endpoint arn:aws:sqs:sa-east-1:000000000000:toll-records-created-notifications --region sa-east-1
+
 # Purge SQS
 docker exec -it localstack awslocal sqs purge-queue --queue-arn arn:aws:sqs:sa-east-1:000000000000:toll-records-payment-pending-queue
--- src/main/java/br/com/vitormelonunes/toll_records/adapters/outbound/message/TollRecordTopicMessage.java
+++ src/main/java/br/com/vitormelonunes/toll_records/adapters/outbound/message/TollRecordTopicMessage.java
@@ -1,5 +1,6 @@
 package br.com.vitormelonunes.toll_records.adapters.outbound.message;
 
+import io.awspring.cloud.sns.core.SnsNotification;
 import io.awspring.cloud.sns.core.SnsTemplate;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
@@ -12,7 +13,7 @@ public class TollRecordTopicMessage {
         this.snsTemplate = snsTemplate;
     }
 
-    void sendNotification(String topicArn, String message) {
-       snsTemplate.sendNotification(topicArn, message, null);
+    void sendNotification(String topicArn, SnsNotification message) {
+        snsTemplate.sendNotification(topicArn, message);
     }
 }
